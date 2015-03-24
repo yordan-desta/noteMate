cfdb1e5f9df8f15efba6a71946c2cc3d3ab4a693
yordan <me@gmail.com>
2015-03-24 12:12:21 +0300

    background color and css stypling changed

--- mainwindow.py
+++ mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '.\mainwindow.ui'
 #
-# Created: Tue Mar 24 11:38:06 2015
+# Created: Tue Mar 24 12:10:41 2015
 #      by: PyQt4 UI code generator 4.11.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -32,23 +32,31 @@ class Ui_Dialog(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
         Dialog.setSizePolicy(sizePolicy)
+        Dialog.setStyleSheet(_fromUtf8(""))
         self.topicCombobox = QtGui.QComboBox(Dialog)
         self.topicCombobox.setGeometry(QtCore.QRect(40, 2, 261, 26))
+        self.topicCombobox.setStyleSheet(_fromUtf8("color: rgb(0, 85, 255);\n"
+"font: 75 9pt \"Microsoft JhengHei UI\";\n"
+""))
         self.topicCombobox.setObjectName(_fromUtf8("topicCombobox"))
         self.addButton = QtGui.QPushButton(Dialog)
         self.addButton.setGeometry(QtCore.QRect(306, 2, 31, 26))
-        self.addButton.setText(_fromUtf8(""))
+        self.addButton.setStyleSheet(_fromUtf8("color: rgb(42, 42, 255);\n"
+""))
         self.addButton.setObjectName(_fromUtf8("addButton"))
         self.delButton = QtGui.QPushButton(Dialog)
         self.delButton.setGeometry(QtCore.QRect(3, 2, 31, 26))
-        self.delButton.setText(_fromUtf8(""))
+        self.delButton.setStyleSheet(_fromUtf8("color: rgb(203, 5, 5);"))
         self.delButton.setObjectName(_fromUtf8("delButton"))
         self.textEdit = QtGui.QPlainTextEdit(Dialog)
         self.textEdit.setGeometry(QtCore.QRect(4, 30, 337, 451))
+        self.textEdit.setStyleSheet(_fromUtf8("font: italic 12pt \"MS Sans Serif\";\n"
+"background-color: rgb(255, 246, 107);"))
         self.textEdit.setObjectName(_fromUtf8("textEdit"))
         self.saveButton = QtGui.QPushButton(Dialog)
         self.saveButton.setGeometry(QtCore.QRect(301, 450, 35, 27))
-        self.saveButton.setText(_fromUtf8(""))
+        self.saveButton.setStyleSheet(_fromUtf8("font: italic 9pt \"MS Shell Dlg 2\";\n"
+"color: rgb(85, 85, 255);"))
         self.saveButton.setObjectName(_fromUtf8("saveButton"))
 
         self.retranslateUi(Dialog)
@@ -60,4 +68,7 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(_translate("Dialog", "Dialog", None))
+        self.addButton.setText(_translate("Dialog", "+", None))
+        self.delButton.setText(_translate("Dialog", "-", None))
+        self.saveButton.setText(_translate("Dialog", "save", None))
 
