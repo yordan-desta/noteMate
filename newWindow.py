1c8a19c7e765aa395a3df6ce66375a43c27f45ca
pyordan <yordan.desta@gmail.com>
2015-03-24 09:22:40 +0300

    delete bug fixed and code cleanup performed

--- newWindow.py
+++ newWindow.py
@@ -1,36 +1,28 @@
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-import sys
+import os
 from mainwindow import *
-import thread
-import time
 import splash
 class mainWindow(QDialog,Ui_Dialog):
     def __init__(self,parent=None):
         self.wordCount=0
         super(mainWindow,self).__init__(parent)
         self.setupUi(self)
-        #e= self.height()
-        #self.resizeEvent()
         self.setFixedWidth(self.width())
         self.setFixedHeight(self.height())
         self.check_size(self)
         self.setGeometry(1000,0,342, 481)
-
         self.delButton.setText("<-")
         self.addButton.setText("+")
         self.saveButton.setText("save")
         self.textEdit.appendPlainText("")
         self.textEdit.setFocus()
         self.setWindowTitle("noteMate(test Release)")
-
     def startUpSetup(self):
-
         self.topicCombobox.addItems(self.topicList)
         for i,topics in enumerate(self.topicList):
             if topics==self.callerButId:
                 self.topicCombobox.setCurrentIndex(i)
-        #create if file not exist
         topic=self.topicCombobox.currentText()
         self.setupTopicWindow(topic)
         self.connect(self.delButton,SIGNAL("clicked()"),self.deleteCurrentTopic)
@@ -41,7 +33,6 @@ class mainWindow(QDialog,Ui_Dialog):
             self.delButton.setEnabled(False)
         else:
             self.delButton.setEnabled(True)
-
     def saveCurrentTopicText(self):
         topic=self.topicCombobox.currentText()
         try:
@@ -50,55 +41,34 @@ class mainWindow(QDialog,Ui_Dialog):
             file.write(topicText)
             """display saved msg and disable save button"""
             file.close()
-
         except:
             pass
-
-
     def check_size(self,x):
-        print "resized"
         while self.windowState():
            self.textEdit.setFixedHeight(self.height())
            self.textEdit.setFixedWidth(self.width())
-
-
     def topicItems(self,topicList,callerButId):
-
         self.topicList= topicList
         self.callerButId=callerButId
         self.startUpSetup()
-
     def getTopicFileText(self,topic):
-
         try:
             file=open(topic,'r')
             text= file.read()
             file.close()
             return text
-
         except:
             return ValueError
-
-
     def backToSplash(self):
         self.sd=splash.splashDialog()
         self.sd.setupUI()
         self.sd.setModal(True)
         self.sd.show()
-        #self.sd.exec_()
         self.hide()
-
     def updateTextEdit(self,currentTopic):
-        #save previous textedit
-        #self.saveCurrentTopicText()
-        #cleanup edit text
         self.textEdit.clear()
         if len(self.topicList)>0:
-
-            #show the text for current topic
-            print len(self.topicList)
             self.setupTopicWindow(currentTopic)
-
     def setupTopicWindow(self,topic):
         for i,t in enumerate(self.topicList):
             if t==topic:
@@ -106,38 +76,27 @@ class mainWindow(QDialog,Ui_Dialog):
                 break
         try:
             file=open(topic,'r')
-            print "the file for %s exists"%(self.topicCombobox.currentText())
             file.close()
             topicText=self.getTopicFileText(topic)
             self.textEdit.setPlainText(topicText)
         except:
             file=open(topic,'w')
-            print "the file for %s is created for the first time"%(self.topicCombobox.currentText())
             file.close()
     def deleteCurrentTopic(self):
         topic=self.topicCombobox.currentText()
         topicIndex=self.topicCombobox.currentIndex()
         self.deleteTopic(topic,topicIndex)
     def deleteTopic(self,topic,index=0):
-        #delete from newWindow
-
         for i,t in enumerate(self.topicList):
             if t==topic and i==index:
                 self.topicList.remove(topic)
                 break
-
-        #delete from splash List
         file=open("arrayList",'r')
-
-
         self.topicCombobox.removeItem(index)
         self.delFromSplash(topic)
-        #self.topicCombobox.setCurrentIndex(index-1)
-        #file=open(topic)
-        #del file
-        print len(self.topicList),self.topicList
-        if(len(self.topicList)>0):
+        os.remove(topic)
 
+        if(len(self.topicList)>0):
             topic=self.topicList[0]
             self.setupTopicWindow(topic)
             self.delButton.setEnabled(True)
@@ -145,11 +104,6 @@ class mainWindow(QDialog,Ui_Dialog):
             self.saveButton.setEnabled(False)
             self.topicCombobox.setEnabled(False)
             self.delButton.setEnabled(False)
-
-        #self.startUpSetup()
-        #print self.topicList
-        #print self.callerButId
-
     def delFromSplash(self,topic):
         file=open("arrayList",'r')
         r=file.read()
@@ -166,5 +120,4 @@ class mainWindow(QDialog,Ui_Dialog):
                     file.write(t+",")
                 else:
                     file.write(t)
-
         file.close()
--- splash.py
+++ splash.py
@@ -3,7 +3,6 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 import sys
 from newWindow import *
-
 buttonArray=[]
 topicTitle=""
 topicIsAdded=False
@@ -12,38 +11,28 @@ selfinitFlag=True
 savedTopics=True
 class splashDialog(QDialog):
     def setupUI(self):
-
         self.initTopics()
         self.countForAddtopicEntry=0
         self.addButtonIsClicked=False
         self.topicCount=0
-
         global topics,topicIsAdded,topicTitle,buttonArray,savedTopics
-
         self.vlayMain=QVBoxLayout()
         self.hlay=QHBoxLayout()
         self.qlable=QLabel("Select your topic")
         self.addbut=QPushButton("add")
-
         self.vlay=QVBoxLayout()
-
         self.setWindowTitle("noteMate V0.1")
         self.hlay.addWidget(self.qlable)
         self.hlay.addWidget(self.addbut)
-
         self.vlayMain.addLayout(self.hlay)
-
         self.setLayout(self.vlayMain)
-
         self.setWindowTitle("noteMate")
-
         if savedTopics and (len(topics)!=0 or (topicIsAdded and topicTitle!="")):
             topicIsAdded=False
             self.addTopic(topicTitle)
             topicTitle=""
 
         self.connect(self.addbut,SIGNAL("clicked()"),self.addButtonClicked)
-
 
     def addTopic(self,topictitle="Topic"):
         """resetting button array list"""
@@ -64,9 +53,8 @@ class splashDialog(QDialog):
                 buttonArray.append(topBut)
         for i,t in enumerate(topics):
             if t=="":
-                #print "empty"
                 del topics[i]
-        print "*****",topics,"*****"
+
         self.saveTopics()
         for i,objectname in enumerate(buttonArray):
 
@@ -74,13 +62,10 @@ class splashDialog(QDialog):
         self.countForAddtopicEntry+=1
     def addButtonClicked(self):
         self.addButtonIsClicked=True
-       # print self.addButtonIsClicked
         self.atui=addTopicUi()
         self.hide()
         self.atui.setupUi()
-
     def startwindow(self,buttonObjName):
-        #print buttonObjName
         self.nw= mainWindow()
         self.nw.topicItems(topics,buttonObjName)
         self.nw.show()
@@ -98,10 +83,8 @@ class splashDialog(QDialog):
                     global savedTopics
                     savedTopics=False
                 for topic in rtopics:
-                    topics.append(topic)
-                    #print topic
-                file.close()
-                #print topics
+                 topics.append(topic)
+                 file.close()
                 self.saveTopics()
             except:
                 file=open("arrayList",'w+')
@@ -112,17 +95,12 @@ class splashDialog(QDialog):
         file=open("arrayList",'w+')
         count=0
         for topic in topics:
-           # print topic
             if count==0:
                 file.write(topic)
                 count+=1
             else:
                 file.write(","+topic)
         file.close()
-
-
-
-
 class addTopicUi(QDialog):
     def setupUi(self):
         self.hlay=QHBoxLayout()
@@ -136,25 +114,19 @@ class addTopicUi(QDialog):
         self.connect(self.okbut,SIGNAL("clicked()"),self.addtopic)
     def addtopic(self):
         global topicTitle,topicIsAdded,topics
-        #print topicIsAdded
         global savedTopics
         savedTopics=True
         topicTitle=self.line.text()
         topicIsAdded=True
-        #print topicTitle
         self.sd=splashDialog()
         self.sd.setupUI()
         self.hide()
         self.sd.show()
-        #self.sd.exec_()
-
 
 class topicButtons(QPushButton):
     def __init__(self,parent=None):
         super(topicButtons,self).__init__(parent)
         self.connect(self,SIGNAL("clicked()"),self.topicClicked)
-
     def topicClicked(self):
         self.objName=str(self.objectName())
-        #print type(self.objName)
         self.emit(SIGNAL("topic_clicked(QString)"),self.objName)
